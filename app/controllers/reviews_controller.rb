d25f5892149f7b98235d9088b2c24b9dcd56caa9
Mark Crelley <markcrelley2010@googlemail.com>
2021-05-29 17:01:04 +0800

    yelp clone completed, need to finish styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+	before_action :find_restaurant, only: [:new, :create]
+
+	def new
+		@review = Review.new
+	end
+
+	def create
+		review_params = params.require(:review).permit(:content, :rating)
+		@review = Review.new(review_params)
+		@review.restaurant = @restaurant
+		if @review.save
+   		redirect_to restaurant_path(@restaurant)
+   	else
+   		render :new
+   	end
+	end
+
+	def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+	private
+
+	def find_restaurant
+		@restaurant = Restaurant.find(params[:restaurant_id])
+	end
+
 end
